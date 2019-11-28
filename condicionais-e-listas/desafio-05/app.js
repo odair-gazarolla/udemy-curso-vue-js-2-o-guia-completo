e9bf840b7020b4bbd806789576c73ea797d13ce2
Odair Gazarolla <odair.gazarolla@rkmsistemas.com.br>
2019-11-28 14:24:23 -0300

    Conditions and list rederization

---
+++ condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,24 @@
+new Vue({
+
+	el: "#desafio",
+	data: {
+
+		mostrarp1: true,
+
+		livro: {
+
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R Tolkiens',
+			volume: '3'
+		},
+
+		aluno: {
+
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9,21]
+		},
+
+		frutas: ['abacate', 'mamão', 'jaboticaba', 'abacaxi', 'morango', 'uva']
+	}
+});
