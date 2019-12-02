90e5e8140c83fe070f4dafb5a99b5ecdc3c7ae79
Odair Gazarolla <odair.gazarolla@rkmsistemas.com.br>
2019-12-02 15:05:00 -0300

    Fluxo de desenvolvimento real usando Vue CLI - concluido

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data: function() {
+
+      return {
+
+        titulo: 'Teste Data usando Vue JS!!'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+
+    background-color: chocolate;
+    color: #fff;
+  }  
+</style>
